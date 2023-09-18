744840ba96e89495dd3d820465d2bfa3610cebc6
Nayem Islam <143949943+nayemislam2023@users.noreply.github.com>
2023-09-18 17:55:15 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function(){
+	/*$('.slider').slick({
+		infinite:true,
+		slidesToShow:3,
+		slidesToScroll:1,
+		arrows:false,
+		autoplay:true,
+		autoplaySpeed:500,
+		
+	});*/
+	
+	
+	$('.slider-slick').slick({
+		infinite:true,
+		slidesToShow:1,
+		slidesToScroll:1,
+		arrows:true,
+		autoplay:true,
+		autoplaySpeed:800,
+		fade :true,
+		prevArrow : '<i class="fa fa-arrow-left"></i>',
+		nextArrow : '<i class="fa fa-arrow-right"></i>',
+		dots : false,
+		
+	});
+	
+	
+	
+	
+})
